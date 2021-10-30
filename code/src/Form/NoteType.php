b6f351c0ec78508875d2f6473afbfe7c386144f8
vins-stha <shrs_samar@yahoo.com>
2021-10-30 15:28:47 +0300

    note crud

---
+++ code/src/Form/NoteType.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Note;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\NotNull;
+
+class NoteType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('title', TextType::class,[
+                'constraints' => [
+                    new NotNull()
+                ]
+            ])
+            ->add('text', TextType::class,[
+                'constraints' => [
+                    new NotNull()
+                ]
+            ])
+            ->add('created_time',DateTimeType::class,[
+                    'widget' =>'single_text',
+                ]
+            )
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Note::class,
+        ]);
+    }
+}
