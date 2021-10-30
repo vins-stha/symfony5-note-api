b34f0396730805aa32d7c26ff649b0033090df43
vins-stha <shrs_samar@yahoo.com>
2021-10-30 20:18:35 +0300

    note api controller endpoints added for update,delete

--- code/src/Controller/NoteApiController.php
+++ code/src/Controller/NoteApiController.php
@@ -6,7 +6,6 @@ namespace App\Controller;
 use App\Entity\Note;
 use App\Form\NoteType;
 use App\Repository\NoteRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use App\Controller\AbstractFormClass;
@@ -23,9 +22,11 @@ class NoteApiController extends AbstractApiController
   public function createAction(Request $request): Response
   {
     $note = new Note();
+
     $data = json_decode($request->getContent(), true);
 
     $form = $this->createForm(NoteType::class, $note);
+
     $form->submit($data);
 
     if (!$form->isValid()) {
@@ -35,12 +36,94 @@ class NoteApiController extends AbstractApiController
     }
 
     $em = $this->getDoctrine()->getManager();
+
     $em->persist($note);
+
     $em->flush();
 
     $response = new Response($this->json($note));
+
     $response->setStatusCode(Response::HTTP_OK);
 
     return $response;
   }
+
+  public function findByIdAction(Request $request):Response
+  {
+    $id = $request->get('id');
+
+    $note = $this->getDoctrine()->getRepository(Note::class)->findOneBy([
+        'id' => $id
+    ]);
+
+    if (!$note)
+    {
+      throw new NotFoundHttpException('Note with id '.$id. ' not found!');
+    }
+
+    return $this->json($note);
+  }
+
+  public function updateByIdAction(Request $request):Response
+  {
+    $id = $request->get('id');
+
+    $data = json_decode($request->getContent(), true);
+
+    $note = $this->getDoctrine()->getRepository(Note::class)->findOneBy([
+        'id' => $id
+    ]);
+
+    if (!$note)
+    {
+      throw new NotFoundHttpException('Note with id '.$id. ' not found!');
+    }
+
+    $form = $this->createForm(NoteType::class, $note, [
+
+    ]);
+
+    $form->submit($data);
+
+    if (!$form->isSubmitted() || !$form->isValid())
+    {
+      return $this->respond($form,Response::HTTP_BAD_REQUEST);
+    }
+
+    $em = $this->getDoctrine()->getManager();
+    $em->persist($note);
+    $em->flush();
+
+    $response =  new Response($this->json($note));
+    $response->setStatusCode(Response::HTTP_OK);
+
+    return $response;
+
+  }
+
+  public function deleteByIdAction(Request $request):Response
+  {
+    $id = $request->get('id');
+
+    $note = $this->getDoctrine()->getRepository(Note::class)->findOneBy([
+        'id' => $id
+    ]);
+
+    if (!$note)
+    {
+      throw new NotFoundHttpException('Note with id '.$id. ' not found!');
+    }
+
+    $em = $this->getDoctrine()->getManager();
+    $em->remove($note);
+    $em->flush();
+
+    $response = new Response();
+    $response->setStatusCode(Response::HTTP_OK);
+    $message = 'Deleted Successfully!';
+    return $response->setContent($this->json($message));
+//    return print($this->json($id));
+
+  }
+
 }
